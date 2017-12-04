e75a6502afea6738c032ee87de557865c73eff94
Brittany Morris <brittmmendez@gmail.com>
2017-12-04 18:37:02 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,11 +44,10 @@ end
 def add_s(array)
   new=[]
   array.each_with_index.collect do|element, index|
-  if index!=1
+    if index!=1
         "#{element}s"
-else
-  element
-
-end
-end
+      else
+        element
+      end
+    end
 end
